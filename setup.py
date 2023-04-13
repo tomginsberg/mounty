010cb00eab9d5e1531c69e62a7fb3ef809739edd
Tom Ginsberg <tomginsberg@cs.toronto.edu>
2023-04-13 01:11:35 -0400

    mounty.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='mounty',
     version='0.1',
-    py_modules=['mounty.py'],
+    py_modules=['mounty'],
     install_requires=open('requirements.txt').readlines(),
     entry_points={
         'console_scripts': [
